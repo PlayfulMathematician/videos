ef64c35d1fbfedcf560ba25cedfd8114a3d65767
PlayfulMathematician <me@playfulmathematician.com>
2025-07-04 17:03:44 -0400

    aggressiveness

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("Input your age: ");
+        io::stdout().flush().unwrap();
+        let mut age = String::new();
+        io::stdin().read_line(&mut age).expect("No");
+        let age: i8 = match age.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("You absolute failure");
+                    break;
+                }
+        };
+        if age < 18 {
+            println!("child");
+            break;
+        }
+        println!("adult");
+        break; 
+    }
 }
