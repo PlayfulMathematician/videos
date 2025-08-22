199c0eb72ad6651f8f998e2844267102e8d52c69
playfulmathematician <me@playfulmathematician.com>
2025-08-22 15:39:19 -0600

    made it even better

--- uniformity/src/main.c
+++ uniformity/src/main.c
@@ -1,19 +1,20 @@
 /*
-main.c - A program that does off parsing and rendering and stuff
+    main.c - A program that handles most things
     Copyright (C) 2025 PlayfulMathematician
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 */
 
 #include <stdio.h>
@@ -908,10 +909,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         glColor3f(0,0,1); 
         glVertex3f(0.0f,0.5f,0.0f);
         glEnd();
-
         SwapBuffers(hdc);
     }
-
     cleanup:
         wglMakeCurrent(NULL, NULL);
         wglDeleteContext(hglrc);
